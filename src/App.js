842e917ccab428575e234b5e3957b49ed8ba4fe5
sungeunp <sungeun1214@gmail.com>
2025-10-20 22:54:49 +0900

    feat(input/output): 초기 입출력 스켈레톤 구현
    - `@woowacourse/mission-utils` 패키지의 `Console` 객체를 사용하여 `초기 질문 출력`, `문자열 입력` 기능을 수행하도록 한다.
    - 마찬가지로 예시 결과 출력도 `Console` 객체를 사용하여 요구사항대로 구현하고자 한다.
    - 예시 결과까지 출력하는 이유는 프로그램의 전체적인 뼈대를 잡고, 핵심 기능에 집중하기 위함이다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    // 사용자의 입력을 받는다.
+    const userInput = await Console.readLineAsync(
+      "덧셈할 문자열을 입력해 주세요. \n"
+    );
+
+    // 계산을 수행한다.
+    const result = 0;
+
+    // 계산된 결과를 출력한다. (현재는 예시 결과 출력)
+    Console.print(`결과 : ${result}`);
+  }
 }
 
 export default App;
